6e763ea71950637554d422806ceca1a4beb42193
Jaimie Swisher <Jaimieswisher@yahoo.com>
2021-02-03 17:59:48 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,24 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Enter the name of item");
+    var name = Console.ReadLine();  
+
+    Console.Writeline ("Enter the quantity");
+    var x = 6
+    Console.ReadLine(x);
+    
+    Console.WriteLine ("Enter the price");
+    var y = 10
+    Console.ReadLine(y);
+
+    Intx1 = ConvertToInt32(x);
+
+    Inty1 = ConvertToInt32(y);
+
+    Console.Writeline ("Total price of apple is $");
+    Console.ReadLine((x1*y1));
+
+  }
+}
